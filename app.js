61db6bfe8e2f0743be43842d81317bb5cee2e9e9
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-12 11:20:58 +0200

    Add a basic layout in css using semantic-ui

--- app.js
+++ app.js
@@ -8,20 +8,18 @@ $("#title-button").on("click", function(){
 	console.log($("#title-input").val());
 	if ($("#title-input").val() !== ""){
 		titles[titleCount] = $("#title-input").val();
-		var inject = "<div id='title" + titleCount + "' data-title_nbr='" + titleCount + "' ><h2>"+ titles[titleCount] +"</h2></div>";
+		var inject = "<div id='title" + titleCount + "' class='dynamicTitle' data-title_nbr='" + titleCount + "' ><span>"+ titles[titleCount] +"</span></div>";
 		$("#listTitle").append(inject);
 		currentTitle = titles[titleCount];
 		titleCount++;
 		display();
-		/*$("#current-title").html(currentTitle);*/
 		console.log(titles,"titles array", currentTitle, "currentTitle");
 	}
 });
 
 $("#listTitle").on("click", "div", function(){
 	var titleNbr = $(this).data("title_nbr");
-	currentTitle = titles[titleNbr];
-	/*$("#current-title").html(currentTitle);*/
+	currentTitle = titles[titleNbr];	
 	console.log(titleNbr);
 	console.log(titles,"titles array", currentTitle, "currentTitle");
 	display();
@@ -50,7 +48,7 @@ $("#task-button").on("click", function(){
 	todos[listCount].title = currentTitle;
 	todos[listCount].status = true;
 	console.log(todos);
-	var inject = "<div id=div" + listCount + " data-tasknbr='" + listCount + "'><input type=checkbox id='checkbox" + listCount + "' data-tasknbr='" + listCount + "'><span id='" + listCount +  "' data-taskNbr='" + listCount + "' class= >" + todos[listCount].task  + "</span></div>";
+	var inject = "<div id=div" + listCount + " data-tasknbr='" + listCount + "'><input type=checkbox class ='checky' id='checkbox" + listCount + "' data-tasknbr='" + listCount + "'><span id='" + listCount +  "' data-taskNbr='" + listCount + "' class= >" + todos[listCount].task  + "</span></div>";
 	$("#list-task").append(inject);
 	listCount++;
 });
@@ -61,7 +59,7 @@ function display(x){
 		$("#list-task").html("");
 		for (var i = 0; i < todos.length; i++) {
 			if (todos[i].status === false && todos[i].title == currentTitle){
-				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class='checked'>"+ todos[i].task  + "</span></div>";
+				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox class ='checky' id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class='checked'>"+ todos[i].task  + "</span></div>";
 				$("#list-task").append(inject);
 			}
 		}
@@ -70,7 +68,7 @@ function display(x){
 		$("#list-task").html("");
 		for (var i = 0; i < todos.length; i++) {
 			if (todos[i].status === true && todos[i].title == currentTitle){
-				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class=''>"+ todos[i].task  + "</span></div>";
+				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox class ='checky' id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class=''>"+ todos[i].task  + "</span></div>";
 				$("#list-task").append(inject);
 			}
 		}
@@ -96,12 +94,12 @@ function display(x){
 			console.log(i , "loop|||", todos[i].title, "index title");
 			if (todos[i].status === false && todos[i].title == currentTitle){
 				console.log("loop if");
-				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class='checked'>"+ todos[i].task  + "</span></div>";
+				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox class ='checky' id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class='checked'>"+ todos[i].task  + "</span></div>";
 				$("#list-task").append(inject);
 			}
 			else if(todos[i].status === true && todos[i].title == currentTitle) {
 				console.log("loop else if");
-				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class=''>"+ todos[i].task  + "</span></div>";
+				var inject = "<div id=div" + i + " data-tasknbr='" + i + "'><input type=checkbox class ='checky' id='checkbox" + i + "' data-tasknbr='" + i + "'><span id='" + i +  "' data-taskNbr='" + i + "' class=''>"+ todos[i].task  + "</span></div>";
 				$("#list-task").append(inject);
 			}
 		}
